2e612a34e5ec296cf8fb23a1500f8ef3ad0545da
ben_dev <bernard.kolnrekaj@gmail.com>
2022-08-09 04:34:51 +0200

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,185 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Custom CSS -->
+    <link rel="stylesheet"href="./style.css">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- FOntawesome CDN -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+    <title>Dentamente</title>
+  </head>
+  <body>
+      <div class="container">
+          <div class="row">
+              <div class="col-12 col-lg-6 p-5 d-flex"> 
+              <i class="fas fa-tooth fa-4x text-primary"></i><h2 class="text-primary p-2">Dentamente</h2>
+              </div>
+              <div class="intro col-12 col-lg-6 p-5">
+                  <p>Il tuo dentista di fiducia a Roma</p>
+                  <p>Dentista Giovanna Rossi e colleghi</p>
+              </div>    
+          </div>
+      </div>
+
+
+      <!-- Navbar -->
+
+      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container-fluid">   
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarColor02">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Su di noi</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Trattamenti</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Informazioni generali</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contatti</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+
+  <section class= "container-fluid  p-5">
+      <div class="row sezione ">
+          <div class="col-12">
+              <h2>Cari pazienti</h2>
+              <p><strong>lo studio Dentamente è aperto</strong> ed offre tutti i trattamenti nei consueti orari di apertura.</p> 
+              <p>Vengono seguite e intensificate tutte le procedure di disinfezione necessarie.</p>
+              <p>Se si hanno sintomi influenzali si prega di posticipare l'appuntamento di min.</p> 
+              <p>14 giorni e non visitare il nostro studio. La tua squadra Dentamente</p>
+          </div>
+      </div>
+  </section>
+
+    <div class="container-fluid p-5">
+        <div class="row">
+            <div class="col-12 col-lg-4"><h4>Welcome at Dentamente!</h4> 
+                <p>We would like to welcome you in our dental practice in Rome. 
+              We are english speaking dentists with a professional and motivated team 
+              and would like to give you a feeling of a unstressed atmosphere and a 
+              friendly service. We look forward meeting you.</p></div> 
+                <div class="col-12 col-lg-4">
+                <h4>Benvenuto a Dentamente!</h4>
+                <p>Vorremmo darti il benvenuto nel nostro studio dentistico a Roma. 
+                Siamo dentisti di lingua inglese con un team professionale e motivato 
+                e vorremmo darti la sensazione di un'atmosfera rilassata e un servizio cordiale.
+                Non vediamo l'ora di incontrarti.</p>  
+            </div>
+            <div class="col-12 col-lg-4">
+                <div class="card w-75">
+                    <div class="card-body">
+                        <h5 class="card-title">Orari d'ufficio</h5>
+                        <ul>
+                            <li><strong>Lun.Mar.Mer</strong> : 09-14h & 15-19h</li>
+                            <li><strong>Mercoledí</strong> : 14-20h</li>
+                            <li><strong>Venerdí</strong>: 09-15h</li>
+                            <li><strong>Sabato</strong>: previo accordo</li>
+                        </ul>
+                        <a href="#" class="btn btn-primary">Fissa un appuntamento </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <div class="container-fluid p-5">
+        <div class="row">
+            <div class="col-12 col-lg-3">
+                <h4>Impressioni della nostra pratica</h4>
+                <p>Dai un'occhiata alla nostra pratica e al nostro team </p>
+            </div>
+            <div class="col-12 col-lg-3">
+                <div class="card" style="width: 18rem;">
+                    <img src="http://picsum.photos/300" class="card-img-top" alt="...">
+                    <div class="card-body d-flex justify-content-center">
+                    <label class="btn btn-outline-primary" for="btnradio1">Il nostro team</label>
+                    </div>
+                    
+                </div>
+            </div>
+            <div class="col-12 col-lg-3">
+            <div class="card" style="width: 18rem;">
+                    <img src="http://picsum.photos/200" class="card-img-top" alt="...">
+                    <div class="card-body d-flex justify-content-center">
+                    <label class="btn btn-outline-primary" for="btnradio1">reception</label>
+                    </div>
+                    
+                </div>
+            </div>
+            <div class="col-12 col-lg-3">
+            <div class="card" style="width: 18rem;">
+                    <img src="http://picsum.photos/100" class="card-img-top" alt="...">
+                    <div class="card-body d-flex justify-content-center">
+                    <label class="btn btn-outline-primary" for="btnradio1">Sala del dentista</label>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+
+<!-- Inizio footer -->
+    <footer class="container-fluid bg-primary">
+      <div class="row ">
+          <div class="col-12 col-lg-3 text-white p-3">
+          <i class="fas fa-tooth fa-2x"> </i>
+          <p>Siamo un team internazionale di dentisti nel cuore di Roma,</p>
+          <p>che sarà lieto di aiutarvi con trattamenti e consigli competenti. </p>
+          </div>
+          <div class="col-12 col-lg-3 text-white p-3">
+              <h4>Dettaglio contatti</h4>
+              <ul>
+                  <li><strong>Indirizzo</strong>: via Roma 1, Roma</li>
+                  <li><strong>Tel.</strong>: 0123456789</li>
+                  <li><strong>Fax</strong>: 0123456789</li>
+                  <li><strong>Email</strong>:mail@mail.com </li>
+              </ul>
+          </div>
+          <div class="col-12 col-lg-3 text-white p-3">
+              <h4><strong>Orari d'ufficio</strong></h4>
+              <ul>
+                  <li><strong>Lun.Mar.Mer</strong> : 09-14h & 15-19h</li>
+                  <li><strong>Mercoledí</strong> : 14-20h</li>
+                  <li><strong>Venerdí</strong>: 09-15h</li>
+                  <li><strong>Sabato</strong>: previo accordo</li>
+              </ul>
+          </div>    
+            <div class="col-12 col-lg-3 text-white p-3">
+                    <h4><strong>Appuntamenti</strong></h4>
+                    <p>Saremo felici di aiutarti e consigliarti in una consulenza personale </p>
+                <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+                    <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked>
+                    <label class="btn btn-outline-light" for="btnradio1">Fai un appuntamento </label>
+
+                </div>
+            </div>        
+        </div>
+    </footer>
+  <!-- Fine footer  -->
+
+   <!-- Custom JS -->
+   <script src="./main.js"></script>
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+  </body>
+</html>
